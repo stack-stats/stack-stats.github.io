222b7773ff9a9ece851aa0fb436b72e3d347b289
DilumN <dilumnavanjana@gmail.com>
2017-03-11 19:14:24 +0530

    #2 language chart to work dynamic data. But still those are sample data

--- app/languageChart/languagechart.controller.js
+++ app/languageChart/languagechart.controller.js
@@ -1,16 +1,62 @@
 (function (angular) {
-    'use strict';
+  'use strict';
 
-    var languageChartController = function ($scope) {
-        $scope.labels = ["C#", "C++", "C", "PHP", "Ruby", "Java", "Python","Objective-C", "JavaScript", "Perl", "Scala", "R"];
-        $scope.series = ['Series A', 'Series B'];
-        $scope.data = [
-          [1144, 447, 224, 350, 176, 665, 336, 34, 415, 81, 2, 2]];
-        $scope.colours = ['#72C02C', '#3498DB', '#717984', '#F1C40F'];
-    };
+  var languageChartController = function ($scope, $http) {
 
-    languageChartController.$inject = ['$scope'];
+    $http.get('JSON/limited_tags.json')
+    .then(function(res){
+      $scope.tags = res.data;
+      manageJson();
+      manageData();
+    });
 
-    angular.module('myapp').controller('LanguageChartController', languageChartController);
+    $scope.$watch('selectedYear', function(newVal, oldVal){
+      if ($scope.tags){
+        manageData();
+      }
+    });
+
+    $scope.years = [];
+    $scope.labels = [];
+    $scope.data = [];
+    $scope.selectedYear = {id: "2011", name: "2011"};
+
+    // $scope.labels = ["C#", "C++", "C", "PHP", "Ruby", "Java", "Python","Objective-C", "JavaScript", "Perl", "Scala", "R"];
+    $scope.series = ['Series A', 'Series B'];
+    // $scope.data = [
+    // [1144, 447, 224, 350, 176, 665, 336, 34, 415, 81, 2, 2]];
+    $scope.colours = ['#72C02C', '#3498DB', '#717984', '#F1C40F'];
+
+
+  var manageData = function(){
+    $scope.labels = [];
+    $scope.data = [];
+
+    for (var tag in $scope.tags[$scope.selectedYear.id]) {
+      $scope.labels.push(tag);
+      $scope.data.push($scope.tags[$scope.selectedYear.id][tag]);
+    }
+  }
+
+  var manageJson = function() {
+    var count = 0;
+    for (var year in $scope.tags) {
+      var object = {};
+      object.id = year;
+      object.name = year
+      $scope.years.push(object);
+    }
+  };
+
+  var initialize = function() {
+  };
+
+  initialize();
+
+};
+
+  languageChartController.$inject = ['$scope', '$http'];
+
+  angular.module('myapp').controller('LanguageChartController', languageChartController);
 
 })(angular);
